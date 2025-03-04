09b03da94f9fef73ad9419cb78897aae23f8ad9f
John O'Brien <johnobrien.dev@gmail.com>
2025-03-04 12:10:20 -0500

    feat: add search course route to course routes

--- src/routes/courses.ts
+++ src/routes/courses.ts
@@ -8,7 +8,8 @@ import {
   getCourseById, 
   updateCourse,
   deleteCourse,
-  updateHole
+  updateHole,
+  searchCourses
 } from '../controllers/courseController';
 import { asyncHandler } from '../utils/asyncHandler';
 
@@ -23,6 +24,8 @@ router.get('/', asyncHandler(getCourses));
 router.get('/:id', asyncHandler(getCourseById));
 router.patch('/:id', asyncHandler(updateCourse));
 router.delete('/:id', asyncHandler(deleteCourse));
+// Course search - doesn't require authentication
+router.get('/search', asyncHandler(searchCourses));
 
 // Hole routes
 router.patch('/:courseId/holes/:holeNumber', asyncHandler(updateHole));
